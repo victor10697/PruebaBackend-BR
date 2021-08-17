54950b94d178bc486bfeb3cefbb2316a681b3d4c
Victor Manuel Garcia <vmg106@gmail.com>
2021-08-17 12:19:25 -0500

    ac

--- app-laravel/public/app.js
+++ app-laravel/public/app.js
@@ -1,11 +1,17 @@
 function load() {
   var el = document.getElementById("form-sorteo");
-  el.addEventListener("click", (e) => {
+  el.addEventListener("click", async (e) => {
     e.preventDefault();
 
     var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/vnd.vtex.ds.v10+json");
     myHeaders.append("Content-Type", "application/json");
 
+    //validacion datos
+    if(await validarRegistroExistente(document.getElementById("email").value) != false){
+      alert('registro existe');
+      return;
+    }
     var raw = JSON.stringify({
       "nombre": document.getElementById("nombre").value,
       "apellido": document.getElementById("apellido").value,
@@ -13,13 +19,13 @@ function load() {
     });
 
     var requestOptions = {
-      method: 'GET',
+      method: 'POST',
       headers: myHeaders,
       body: raw,
       redirect: 'follow'
     };
 
-    fetch("https://brandliveco.vtexcommercestable.com.br/api/dataentities/PS/documents", requestOptions)
+    fetch("http://brandliveco.vtexcommercestable.com.br/api/dataentities/PS/documents", requestOptions)
     .then(response => response.json())
     .then(result => {
       // logica respuesta del guardado del registro
@@ -29,3 +35,22 @@ function load() {
 }
 
 document.addEventListener("DOMContentLoaded", load, false);
+
+function validarRegistroExistente(email){
+  var myHeaders = new Headers();
+  myHeaders.append("Cookie", "");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  return fetch(`http://brandliveco.vtexcommercestable.com.br/api/dataentities/PS/search?_fields=nombre,apellido,email&_where=${encodeURI('email='+email)}`, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      // respuesta validacion
+      return result.length > 0 ? true : false;
+    })
+    .catch(error => {return false});
+}
